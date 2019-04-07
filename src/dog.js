cdba1e5c3392969343254e31862488f791aaebe7
your name here <gradyzhoo@gmail.com>
2019-04-07 14:08:04 -0700

    Write StartAni class, sprites rendering

--- src/dog.js
+++ src/dog.js
@@ -1,13 +1,9 @@
+const StartAni = require("./start_ani.js");
+
 class Dog {
   constructor(c) {
     this.c = c;
-    this.currPosX = 0;
-    this.pauseCount = 0;
-    this.pauseCount2 = 0;
-
-    this.shockFin = false;
-    this.jumpStartFin = false;
-    this.jumpEndFin = false;
+    this.startAni = new StartAni(this.c);
 
     this.hitImage = new Image();
     this.hitImage.onload = () => this.hitImageReady = true;
@@ -20,49 +16,15 @@ class Dog {
     this.missImage = new Image();
     this.missImage.onload = () => this.missImageReady = true;
     this.missImage.src = "/Users/grady/Desktop/duckhunt/images/dog_laugh.png";
-
-    // this.sniffFrameIndex = 0;
-    // this.sniffTickCount = 0;
-    // this.sniffTicksPerFrame = 10;
-
-    // this.sniffImage = new Image();
-    // this.sniffImage.onload = () => this.sniffImageReady = true;
-    // this.sniffImage.src = "";
-
-    // this.walkFrameIndex = 0;
-    // this.walkTickCount = 0;
-    // this.walkTicksPerFrame = 6;
-
-    // this.walkImage = new Image();
-    // this.walkImage.onload = () => this.walkImageReady = true;
-    // this.walkImage.src = "/Users/grady/Desktop/duckhunt/images/dog_walk.png";
-
-    // this.shockImage = new Image();
-    // this.shockImage.onload = () => this.shockImageReady = true;
-    // this.shockImage.src = "/Users/grady/Desktop/duckhunt/images/dog_jump.png";
-
-    // this.jumpImage = new Image();
-    // this.jumpImage.onload = () => this.jumpImageReady = true;
-    // this.jumpImage.src = "/Users/grady/Desktop/duckhunt/images/dog_jump.png";
-
-    // this.jumpImage2 = new Image();
-    // this.jumpImage2.onload = () => this.jumpImage2Ready = true;
-    // this.jumpImage2.src = "/Users/grady/Desktop/duckhunt/images/dog_jump.png";
   }
 
   renderFront() {
-    this.renderWalk();
-    this.renderSniff();
-    this.renderJump();
+    this.startAni.render();
   }
 
-  renderSniff() {
-    this.c.drawImage(
-      this.sniffImage, 
-      this.sniffFrameIndex * 120 / 2, 0,
-      60, 60, 
-      this.currPosX, 275, 
-      125, 125);
+  renderBehind() {
+    this.renderHit();
+    this.renderMiss();
   }
 
   renderHit() {
@@ -83,33 +45,8 @@ class Dog {
       111, 111);
   }
 
-  renderWalk() {
-    this.c.drawImage(
-      this.walkImage, 
-      this.walkFrameIndex * 180 / 3, 0,
-      60, 60, 
-      this.currPosX, 275, 
-      125, 125);
-  }
-
-  renderJump() {
-    if (!this.shockFin) {
-      this.c.drawImage(
-        this.jumpImage, 
-        this.jumpFrameIndex * 180 / 3, 0,
-        60, 60, 
-        this.currPosX, 275, 
-        125, 125);
-    }
-    if (this.shockFin) this.renderJumpStart();
-    if (this.jumpStartFin) this.renderJumpEnd();
-  }
-
   update() {
-    this.isShockFin();
-    this.isJumpStartFin();
-    this.isJumpEndFin();
-
+    this.startAni.update();
     this.missTickCount++;
     if (this.missTickCount > this.missTicksPerFrame) {
       this.missTickCount = 0;
@@ -117,61 +54,6 @@ class Dog {
     }
     if (this.missFrameIndex > 1) {
       this.missFrameIndex = 0;
-    }
-
-    this.sniffTickCount++;
-    if (this.sniffTickCount > this.sniffTicksPerFrame) {
-      this.sniffTickCount = 0;
-      this.sniffFrameIndex++;
-    }
-    if (this.sniffFrameIndex > 1) {
-      this.sniffFrameIndex = 0;
-    }
-
-    this.walkTickCount++;
-    if (this.walkTickCount > this.walkTicksPerFrame) {
-      this.walkTickCount = 0;
-      this.walkFrameIndex++;
-    }
-    if (this.walkFrameIndex > 2) {
-      this.walkFrameIndex = 0;
-    }
-
-    this.jumpTickCount++;
-    if (this.jumpTickCount > this.jumpTicksPerFrame) {
-      this.jumpTickCount = 0;
-      this.jumpFrameIndex++;
-    }
-    if (this.jumpFrameIndex > 2) {
-      this.jumpFrameIndex = 0;
-    }
-
-    if (this.currPosX < 60) {
-      this.currPosX++;
-    }
-
-    if (this.currPosX >= 60) {
-      this.sniffImage.src = "/Users/grady/Desktop/duckhunt/images/dog_sniff.png";
-      this.walkImage.src = "";
-      this.pauseCount++;
-    }
-
-    if (this.pauseCount > 70 && this.currPosX < 130) {
-      this.walkImage.src = "/Users/grady/Desktop/duckhunt/images/dog_walk.png";
-      this.sniffImage.src = "";
-      this.currPosX++;
-    }
-
-    if (this.currPosX >= 130) {
-      this.sniffImage.src = "/Users/grady/Desktop/duckhunt/images/dog_sniff.png";
-      this.walkImage.src = "";
-      this.pauseCount2++;
-    }
-
-    if (this.pauseCount2 > 70) {
-      this.sniffImage.src = "";
-      this.jumpImage.src = "/Users/grady/Desktop/duckhunt/images/dog_jump.png";
-      this.pauseCount2++;
     }
   }
 }
--- src/start_ani.js
+++ src/start_ani.js
@@ -1,9 +1,22 @@
 class StartAni {
   constructor(c) {
     this.c = c;
-    this.shockFin = false;
-    this.jump1Fin = false;
-    this.jump2Fin = false;
+    this.dogPosX = 0;
+    this.dogPosY = 275;
+
+    //walk and sniff
+    this.sniff1Fin = false;
+    this.sniff2Fin = false;
+    this.sniff1Count = 0;
+    this.sniff2Count = 0;
+
+    this.walk1Fin = false;
+    this.walk2Fin = false;
+    this.walk1Count = 0;
+    this.walk2Count = 0;
+
+    this.pause1Count = 0;
+    this.pause2Count = 0;
 
     this.sniffFrameIndex = 0;
     this.sniffTickCount = 0;
@@ -11,7 +24,7 @@ class StartAni {
 
     this.sniffImage = new Image();
     this.sniffImage.onload = () => this.sniffImageReady = true;
-    this.sniffImage.src = "";
+    this.sniffImage.src = "/Users/grady/Desktop/duckhunt/images/dog_sniff.png";
 
     this.walkFrameIndex = 0;
     this.walkTickCount = 0;
@@ -21,48 +34,177 @@ class StartAni {
     this.walkImage.onload = () => this.walkImageReady = true;
     this.walkImage.src = "/Users/grady/Desktop/duckhunt/images/dog_walk.png";
 
-    this.shockImage = new Image();
-    this.shockImage.onload = () => this.shockImageReady = true;
-    this.shockImage.src = "/Users/grady/Desktop/duckhunt/images/dog_jump.png";
+    //jump
+    this.shockFin = false;
+    this.jumpStartFin = false;
+    this.jumpEndFin = false;
+
+    this.shockCount = 0;
+    this.jumpStartCount = 0;
+    this.jumpEndCount = 0;
 
     this.jumpImage = new Image();
     this.jumpImage.onload = () => this.jumpImageReady = true;
     this.jumpImage.src = "/Users/grady/Desktop/duckhunt/images/dog_jump.png";
-
-    this.jumpImage2 = new Image();
-    this.jumpImage2.onload = () => this.jumpImage2Ready = true;
-    this.jumpImage2.src = "/Users/grady/Desktop/duckhunt/images/dog_jump.png";
   }
 
   render() {
-
+    if (!this.sniff2Fin) {
+      this.renderWalkAndSniff();
+    } else {
+      this.renderJump();
+    }
   }
 
   update() {
+    this.isWalk1Fin();
+    this.isWalk2Fin();
+    this.isSniff1Fin();
+    this.isSniff2Fin();
+    this.isShockFin();
+    this.isJumpStartFin();
+    this.isJumpEndFin();
+
+    this.updateSniff();
+    this.updateWalk();
 
   }
 
+  //walk and sniff render functions
+  renderWalkAndSniff() {
+    if (!this.walk1Fin) {
+      this.renderWalk();
+      this.dogPosX++;
+      this.walk1Count++;
+    }
+    if (this.walk1Fin && !this.sniff1Fin) {
+      this.renderSniff();
+      this.sniff1Count++;
+    }
+    if (this.walk1Fin && this.sniff1Fin && !this.walk2Fin) {
+      this.renderWalk();
+      this.dogPosX++;
+      this.walk2Count++;
+    }
+    if (this.walk1Fin && this.sniff1Fin && this.walk2Fin) {
+      this.renderSniff();
+      this.sniff2Count++;
+    }
+  }
+
+  renderWalk() {
+    this.c.drawImage(this.walkImage, 
+      this.walkFrameIndex * 180 / 3, 0,
+      60, 60, 
+      this.dogPosX, 275, 
+      125, 125);
+  }
+
+  renderSniff() {
+    this.c.drawImage(this.sniffImage, 
+      this.sniffFrameIndex * 120 / 2, 0,
+      60, 60, 
+      this.dogPosX, 275, 
+      125, 125);
+  }
+  
+  // sprite update functions
+  updateSniff() {
+    this.sniffTickCount++;
+    if (this.sniffTickCount > this.sniffTicksPerFrame) {
+      this.sniffTickCount = 0;
+      this.sniffFrameIndex++;
+    }
+    if (this.sniffFrameIndex > 1) {
+      this.sniffFrameIndex = 0;
+    }
+  }
+
+  updateWalk() {
+    this.walkTickCount++;
+    if (this.walkTickCount > this.walkTicksPerFrame) {
+      this.walkTickCount = 0;
+      this.walkFrameIndex++;
+    }
+    if (this.walkFrameIndex > 2) {
+      this.walkFrameIndex = 0;
+    }
+  }
+
+  // jump render functions
   renderShock() {
-
-  }
-
-  renderJump1() {
-
-  }
-
-  renderJump2() {
-
-  }
-
-  isShockFin() {
+    this.c.drawImage(this.jumpImage, 
+      0, 0,
+      60, 60, 
+      this.dogPosX, 275, 
+      125, 125);
+    }
     
+  renderJumpStart() {
+    this.c.drawImage(this.jumpImage, 
+      60, 0,
+      60, 60, 
+      this.dogPosX, this.dogPosY, 
+      125, 125); 
+    }
+    
+  renderJumpEnd() {
+    this.c.drawImage(this.jumpImage, 
+      120, 0,
+      60, 60, 
+      this.dogPosX, this.dogPosY, 
+      125, 125);
+    }
+        
+  renderJump() {
+    if (!this.shockFin) {
+      this.c.drawImage(this.jumpImage, 
+        0, 0,
+        60, 60, 
+        this.dogPosX, 275, 
+        125, 125);
+      this.shockCount++;
+    }
+    if (this.shockFin && !this.jumpStartFin) {
+      this.renderJumpStart();
+      this.jumpStartCount++;
+    }
+    if (this.jumpStartFin && this.shockFin) {
+      this.renderJumpEnd();
+      this.jumpEndCount++;
+    }
   }
 
+  // jump update functions
+  isWalk1Fin() {
+    if (this.walk1Count > 40) this.walk1Fin = true;
+  }
+  isSniff1Fin() {
+    if (this.sniff1Count > 40) this.sniff1Fin = true;
+  }
+  isWalk2Fin() {
+    if (this.walk2Count > 40) this.walk2Fin = true;
+  }
+  isSniff2Fin() {
+    if (this.sniff2Count > 40) this.sniff2Fin = true;
+  }
+  isShockFin() {
+    if (this.shockCount > 20) {
+      this.shockFin = true;
+      this.dogPosX += 2;
+      this.dogPosY -= 1.5;
+    }
+  }
   isJumpStartFin() {
-
+    if (this.jumpStartCount > 60) {
+      this.jumpStartFin = true;
+      this.dogPosY += 8;
+      this.dogPosX -= 2;
+    }
   }
-
   isJumpEndFin() {
-
+    if (this.jumpEndCount > 20) this.jumpEndFin = true;
   }
 }
+
+module.exports = StartAni;
